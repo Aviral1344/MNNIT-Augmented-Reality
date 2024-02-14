11c0e8ea7bcb3bc6a8c4b02897dd054d5d753f11
Aviral1344 <aviralgupta1344@gmail.com>
2024-02-14 23:16:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express =  require('express');
+const session= require("express-session")
+const app = express();
+const path = require('path');
+
+const dotenv = require('dotenv')
+dotenv.config();
+
+app.set("view engine", "ejs")
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname,"public")))
+app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
+app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
+
+app.listen(3000, ()=>{
+    console.log('visit http://localhost:3000/');
+});
+
+app.get("/", (req, res) =>{
+    res.render("index.ejs");
+})
